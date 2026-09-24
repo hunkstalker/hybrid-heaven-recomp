59cd15cbc732211dba74ac3ff341caa5af10e0f0
Denis Anfruns Millán <daanfruns@gmail.com>
2026-09-24 19:58:32 +0000

    build(version): 0.4.1 - deteccion de ROM por *.z64 + mensaje con hash requerido

--- include/hh.h
+++ include/hh.h
@@ -18,7 +18,7 @@ namespace hh {
     // derivan el string del log (get_version_string) y el recomp::Version del runtime.
     inline constexpr int kVersionMajor = 0;
     inline constexpr int kVersionMinor = 4;
-    inline constexpr int kVersionPatch = 0;
+    inline constexpr int kVersionPatch = 1;
 
     const char* get_version_string();
 
